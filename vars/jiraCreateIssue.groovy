1adce7f936116af0752704e4b0bbfdc17e56c105
Haitham Odeh <80841967+haitham-99@users.noreply.github.com>
2022-07-18 10:35:26 +0300

    Update jiraCreateIssue.groovy

--- vars/jiraCreateIssue.groovy
+++ vars/jiraCreateIssue.groovy
@@ -1,11 +1,11 @@
 def call(Map config=[:]) {
-  def rawBody = libraryResource 'com/planetpope/api/jira/createIssue.json'
-  def binding = [
-    key: "${config.key}",
-    summary: "${config.summary}",
-    description: "${config.description}",
-    issueTypeName: "${config.issueTypeName}"
-  ]
-  def render = renderTemplate(rawBody,binding)
+//   def rawBody = libraryResource 'com/planetpope/api/jira/createIssue.json'
+//   def binding = [
+//     key: "${config.key}",
+//     summary: "${config.summary}",
+//     description: "${config.description}",
+//     issueTypeName: "${config.issueTypeName}"
+//   ]
+//   def render = renderTemplate(rawBody,binding)
   bat('curl -D- -u $JIRA_CREDENTIALS -X POST --data @add-field.txt -H "Content-Type:application/json" https://testecom.atlassian.net/rest/api/2/issue/')
    }
