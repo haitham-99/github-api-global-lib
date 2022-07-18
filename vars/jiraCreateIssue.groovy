06ff899be2d4c705ffcae4864d27772ae231f910
Haitham Odeh <80841967+haitham-99@users.noreply.github.com>
2022-07-18 10:03:21 +0300

    Update jiraCreateIssue.groovy

--- vars/jiraCreateIssue.groovy
+++ vars/jiraCreateIssue.groovy
@@ -7,14 +7,5 @@ def call(Map config=[:]) {
     issueTypeName: "${config.issueTypeName}"
   ]
   def render = renderTemplate(rawBody,binding)
-  bat('curl -D- -u $JIRA_CREDENTIALS -X POST --data "'{
-    "fields": {
-       "project":
-       {
-          "key": "DEP"
-       },
-       "summary": "Issue created from Jenkins",
-       "description": "Tests failed here is the report:http://localhost:8080/job/ecommerce_website_testing/allure",
-       "issuetype": {
-          "name": "Bug"}}}' " -H "Content-Type: application/json" https://testecom.atlassian.net/rest/api/2/issue/')
-            }
+  bat('curl -D- -u $JIRA_CREDENTIALS -X POST --data @add-field.txt -H "Content-Type:application/json" https://testecom.atlassian.net/rest/api/2/issue/')
+   }
